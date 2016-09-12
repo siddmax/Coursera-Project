9a483495a924f8138d562dd972637a1abf794b4d
siddmax <SidSharma@siddharths-macbook-pro.local>
2016-09-11 17:04:01 -0700

    Initial Commit

---
+++ Coursera Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coursera Project
+//
+//  Created by Siddharth Sharma on 9/11/16.
+//  Copyright © 2016 Siddharth Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Coursera Project/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Coursera Project
+//
+//  Created by Siddharth Sharma on 9/11/16.
+//  Copyright © 2016 Siddharth Sharma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
